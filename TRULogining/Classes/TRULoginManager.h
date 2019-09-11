4870c6bb7470c190ae48a57d49453222c519b6aa
xlili <16602149634@163.com>
2019-09-11 15:55:10 +0800

    init project

---
+++ TRULogining/Classes/TRULoginManager.h
@@ -0,0 +1,17 @@
+//
+//  TRULoginManager.h
+//  MyFramework
+//
+//  Created by Xielili on 2019/9/11.
+//  Copyright © 2019 Xie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TRULoginManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TRULogining/Classes/TRULoginManager.m
@@ -0,0 +1,13 @@
+//
+//  TRULoginManager.m
+//  MyFramework
+//
+//  Created by Xielili on 2019/9/11.
+//  Copyright © 2019 Xie. All rights reserved.
+//
+
+#import "TRULoginManager.h"
+
+@implementation TRULoginManager
+
+@end
